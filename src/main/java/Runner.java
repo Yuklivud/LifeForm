ea55da2f59bbb8a1c5da6fdefa4c3970a0f26926
Maksym Shkarbun <suprime28@yahoo.com>
2024-01-23 00:34:16 +0200

    Update runner
    * A little update runner, add run method, collection, static block

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,22 @@
-public class Runner {
-    public void work(){
+import Organisms.Animals.Animal;
+import Service.Initialization;
+import Service.View;
+import Organisms.Organism;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Runner implements Runnable{
+    public Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection = new HashMap<>();
+    public int cellsNumber = 2_000;
+
+    {
+        Initialization initialization = new Initialization();
+        initialization.initSymbols(collection, cellsNumber);
+    }
+    @Override
+    public void run() {
+        View view = new View();
+        view.show(collection);
     }
 }
