412c14e4b3b95c0294d1d0d0470bab706626b531
Maksym Shkarbun <suprime28@yahoo.com>
2024-01-31 08:24:04 +0200

    Implement Organism Reproduction Mechanism
    his commit introduces the Multiply class, which adds functionality for organism reproduction within the ecosystem simulation. The animalMultiply method facilitates the reproduction process by incrementing the count of a specific organism class at a given position. The method retrieves the map representing the organisms at the specified position, increments the count of the specified organism class, and updates the map accordingly. This addition enhances the realism of the ecosystem simulation by incorporating a mechanism for the reproduction of organisms.

--- src/main/java/Service/AnimalMethods/Multiply.java
+++ src/main/java/Service/AnimalMethods/Multiply.java
@@ -6,11 +6,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Multiply {
-    public void animalMultiply(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position){
+    public void animalMultiply(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, Class<? extends Organism> organismClass){
         HashMap<Class<? extends Organism>, Integer> mapToMultiply = collection.get(position);
-        Class<? extends Organism> organismClass = Organism.class;
         int currentInt = mapToMultiply.get(organismClass);
-        int newInt = currentInt + (currentInt / 2);
+        int newInt = currentInt + 1;
         mapToMultiply.put(organismClass, newInt);
         collection.put(position, mapToMultiply);
     }
