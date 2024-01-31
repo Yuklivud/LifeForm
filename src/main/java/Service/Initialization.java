83ca5e81e570f2fd7e991d4dfd16169e740b154c
Maksym Shkarbun <suprime28@yahoo.com>
2024-01-31 08:14:03 +0200

    Initialize Ecosystem with Randomized Organism
    This commit introduces the initSymbols method,
    responsible for initializing a two-level nested
    map representing an ecosystem. The method populates
    the ecosystem with diverse organisms, including predators,
    herbivores, and plants. The distribution of each organism
    within the cells is randomized using a pseudo-random number generator.
    Additionally, a private method createLife is implemented to facilitate
    the creation and random allocation of organisms to each cell. This code
    lays the foundation for a dynamic and varied ecosystem simulation.

--- src/main/java/Service/Initialization.java
+++ src/main/java/Service/Initialization.java
@@ -4,26 +4,26 @@ import Organisms.Animals.Animal;
 import Organisms.Animals.Herbivores.*;
 import Organisms.Animals.Predators.*;
 import Organisms.Organism;
+import Organisms.Plants.Grass;
 import Organisms.Plants.Plant;
 
 import java.util.*;
 
 public class Initialization {
     private Random random = new Random();
-    public void initSymbols(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int cellsNumbers){
+    public void initSymbols(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int cellsNumbers, HashMap<Class<? extends Organism>, Integer> collectionToView){
 
         for(int i = 0; i < cellsNumbers; i++){
-            createLife(collection, i);
+            createLife(collection, i, collectionToView);
         }
     }
 
-    private void createLife(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int numberOfCell){
+    private void createLife(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int numberOfCell, HashMap<Class<? extends Organism>, Integer> collectionToView){
         HashMap<Class<? extends Organism>, Integer> content = new HashMap<>();
-
         // Predators
 
         content.put(Bear.class, random.nextInt(6));
-        content.put(Wolf.class, random.nextInt(31));
+        content.put(Wolf.class, random.nextInt(1, 31));
         content.put(Boa.class, random.nextInt(31));
         content.put(Eagle.class, random.nextInt(21));
         content.put(Fox.class, random.nextInt(31));
@@ -40,12 +40,16 @@ public class Initialization {
         content.put(Mouse.class, random.nextInt(501));
         content.put(Rabbit.class, random.nextInt(151));
         content.put(Sheep.class, random.nextInt(141));
-        content.put(Caterpillar.class, random.nextInt(2001));
+        content.put(Caterpillar.class, random.nextInt(1001));
 
         // Plants
 
-        content.put(Plant.class, random.nextInt(201));
+        content.put(Grass.class, random.nextInt(201));
 
         collection.put(numberOfCell, content);
+
+//        for (Map.Entry<Class<? extends Organism>, Integer> entry : content.entrySet()) {
+//            collectionToView.merge(entry.getKey(), entry.getValue(), Integer::sum);
+//        }
     }
 }
