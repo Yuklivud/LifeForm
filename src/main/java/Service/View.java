656820572c9ed266a766ff1ab73bd721281f30bc
Maksym Shkarbun <suprime28@yahoo.com>
2024-01-31 08:37:15 +0200

    Realize View
    This commit add view. This class show all animals in our area.

--- src/main/java/Service/View.java
+++ src/main/java/Service/View.java
@@ -1,24 +1,12 @@
 package Service;
 
-import Organisms.Animals.Animal;
-import Organisms.Animals.Predators.Wolf;
 import Organisms.Organism;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class View {
-    public void show(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection){
-        int wolvesCount = 0;
-        for(int i = 0; i < collection.size(); i++){
-            if (collection.containsKey(i)) {
-                HashMap<Class<? extends Organism>, Integer> cellContent = collection.get(i);
-                if (cellContent.containsKey(Wolf.class)) {
-                    wolvesCount += cellContent.get(Wolf.class);
-                }
-            }
-        }
-        System.out.println("Количество волков: " + wolvesCount);
-
+    public void show(HashMap<Class<? extends Organism>, Integer> collection){
+        System.out.println(collection.toString());
+        System.out.println("\n\n");
     }
 }
