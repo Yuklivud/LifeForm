3f0de357e61f0de35702a064d1811623e08b6625
Maksym Shkarbun <suprime28@yahoo.com>
2024-01-31 08:12:17 +0200

    Update and realize logic
    * Updated all logic and method in Herbivores and Predators animals.

--- src/main/java/Organisms/Animals/Animal.java
+++ src/main/java/Organisms/Animals/Animal.java
@@ -2,9 +2,13 @@ package Organisms.Animals;
 
 import Organisms.Organism;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public interface Animal extends Organism {
-    void eat();
-    void move();
-    void dead();
-    void multiply();
+
+    void eat(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position);
+    void move(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position);
+    void dead(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position);
+    void multiply(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position);
 }
