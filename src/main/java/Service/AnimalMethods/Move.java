b24d0cacd86fc1e5b0b8f13e7b872135f7841de0
Maksym Shkarbun <suprime28@yahoo.com>
2024-01-31 08:18:44 +0200

    Implement Organism Movement and Position Updates
    This commit introduces a set of methods for handling the movement of organisms within the ecosystem. The move method is responsible for determining the direction of movement (up, right, down, left) based on the specified side and ensuring the validity of the move using corresponding helper methods (isValidUpMove, isValidRightMove, isValidDownMove, isValidLeftMove). Depending on the direction, the organism's position is updated using dedicated methods (goUp, goRight, goDown, goLeft). These methods involve deleting the organism from its current position and adding it to the new position within the ecosystem map. The deleteValueCollection and addValueCollection methods are employed to update the count of the specific organism class in the respective maps. This code enhances the ecosystem simulation by allowing organisms to move and interact within the environment.

--- src/main/java/Service/AnimalMethods/Move.java
+++ src/main/java/Service/AnimalMethods/Move.java
@@ -7,54 +7,54 @@ import java.util.Map;
 
 public class Move {
 
-    public void move(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, int side, int movementSpeed){
+    public void move(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, int side, int movementSpeed, Class<? extends Organism> organismClass){
         if (side == 1 && isValidUpMove(position, movementSpeed)) {
             // Up
-            goUp(collection, position, movementSpeed);
+            goUp(collection, position, movementSpeed, organismClass);
         } else if (side == 2 && isValidRightMove(position, movementSpeed)) {
             // Right
-            goRight(collection, position, movementSpeed);
+            goRight(collection, position, movementSpeed, organismClass);
         } else if (side == 3 && isValidDownMove(position, movementSpeed)) {
             // Down
-            goDown(collection, position, movementSpeed);
+            goDown(collection, position, movementSpeed, organismClass);
         } else if (side == 4 && isValidLeftMove(position, movementSpeed)) {
             // Left
-            goLeft(collection, position, movementSpeed);
+            goLeft(collection, position, movementSpeed, organismClass);
         } else {}
     }
-    public void goUp(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, int movementSpeed){
+    public void goUp(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, int movementSpeed, Class<? extends Organism> organismClass){
         HashMap<Class<? extends Organism>, Integer> mapToDelete = collection.get(position);
-        deleteValueCollection(mapToDelete, Organism.class);
+        deleteValueCollection(mapToDelete, organismClass);
         collection.put(position, mapToDelete);
         HashMap<Class<? extends Organism>, Integer> mapToAdd = collection.get(position - (movementSpeed * 79));
-        addValueCollection(mapToAdd, Organism.class);
+        addValueCollection(mapToAdd, organismClass);
         collection.put(position - (movementSpeed * 79), mapToAdd);
     }
 
-    public void goRight(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, int movementSpeed){
+    public void goRight(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, int movementSpeed, Class<? extends Organism> organismClass){
         HashMap<Class<? extends Organism>, Integer> mapToDelete = collection.get(position);
-        deleteValueCollection(mapToDelete, Organism.class);
+        deleteValueCollection(mapToDelete, organismClass);
         collection.put(position, mapToDelete);
         HashMap<Class<? extends Organism>, Integer> mapToAdd = collection.get(position + 1);
-        addValueCollection(mapToAdd, Organism.class);
+        addValueCollection(mapToAdd, organismClass);
         collection.put(position + 1, mapToAdd);
     }
 
-    public void goLeft(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, int movementSpeed){
+    public void goLeft(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, int movementSpeed, Class<? extends Organism> organismClass){
         HashMap<Class<? extends Organism>, Integer> mapToDelete = collection.get(position);
-        deleteValueCollection(mapToDelete, Organism.class);
+        deleteValueCollection(mapToDelete, organismClass);
         collection.put(position, mapToDelete);
         HashMap<Class<? extends Organism>, Integer> mapToAdd = collection.get(position - 1);
-        addValueCollection(mapToAdd, Organism.class);
+        addValueCollection(mapToAdd, organismClass);
         collection.put(position - 1, mapToAdd);
     }
 
-    public void goDown(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, int movementSpeed){
+    public void goDown(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, int movementSpeed, Class<? extends Organism> organismClass){
         HashMap<Class<? extends Organism>, Integer> mapToDelete = collection.get(position);
-        deleteValueCollection(mapToDelete, Organism.class);
+        deleteValueCollection(mapToDelete, organismClass);
         collection.put(position, mapToDelete);
         HashMap<Class<? extends Organism>, Integer> mapToAdd = collection.get(position + (movementSpeed * 79));
-        addValueCollection(mapToAdd, Organism.class);
+        addValueCollection(mapToAdd, organismClass);
         collection.put(position + (movementSpeed * 79), mapToAdd);
     }
     public void deleteValueCollection(HashMap<Class<? extends Organism>, Integer> collection, Class<? extends Organism> organismClass){
