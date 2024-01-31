095e183b601a16e7eafc35f9c7b894e5bbb41108
Maksym Shkarbun <suprime28@yahoo.com>
2024-01-31 08:21:13 +0200

    Implement Organism Death Handling
    This commit adds the dead method to the codebase, responsible for simulating the death of an organism at a specified position within the ecosystem. The method retrieves the map representing the organisms at the given position, decrements the count of the specified organism class, and updates the map accordingly. Exception handling is implemented to catch any potential errors during this process, and relevant details are printed to the console using e.printStackTrace(). This functionality enhances the realism of the ecosystem simulation by incorporating organism mortality into the system.

--- src/main/java/Service/AnimalMethods/Dead.java
+++ src/main/java/Service/AnimalMethods/Dead.java
@@ -6,12 +6,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Dead {
-    public void dead(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position){
-        HashMap<Class<? extends Organism>, Integer> mapToKill = collection.get(position);
-        Class<? extends Organism> organismClass = Organism.class;
-        int currentInt = mapToKill.get(organismClass);
-        int newInt = currentInt - 1;
-        mapToKill.put(organismClass, newInt);
-        collection.put(position, mapToKill);
+    public void dead(Map<Integer, HashMap<Class<? extends Organism>, Integer>> collection, int position, Class<? extends Organism> organismClass){
+        try{
+            HashMap<Class<? extends Organism>, Integer> mapToKill = collection.get(position);
+            int currentInt = mapToKill.get(organismClass);
+            int newInt = currentInt - 1;
+            mapToKill.put(organismClass, newInt);
+            collection.put(position, mapToKill);
+        } catch (Exception ignored) {}
+
+
     }
 }
